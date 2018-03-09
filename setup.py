0d202514c641d525f8ab0a244feea948cb457093
zshaheen <zshaheen93@gmail.com>
2018-03-08 16:20:32 -0800

    Restructured and added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="zstash",
+    version="0.0.1",
+    author="Chris Golaz, Zeshawn Shaheen",
+    author_email="golaz1@llnl.gov, shaheen2@llnl.gov",
+    description="Long term HPSS archiving software for E3SM",
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    entry_points={
+        'console_scripts': [
+            'zstash=zstash.main:main'
+    ]}
+)
+
