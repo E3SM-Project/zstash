2e4d4d93ffc609597deeef0ad5811c29358965ad
Ryan Forsyth <forsyth2@llnl.gov>
2020-01-15 14:08:29 -0800

    Refactor hpss code and tests
    In zstash/hpss.py:
    - Factor out common code into helper functions.
    - Add option to not use HPSS.
    
    In tests/test.py:
    - Refactor test code to use unittest.
    - Add test for when hpss='none'.

--- zstash/hpss.py
+++ zstash/hpss.py
@@ -2,88 +2,81 @@ from __future__ import print_function, absolute_import
 
 import os.path
 import shlex
-from subprocess import Popen, PIPE
+import subprocess
 from .settings import logger
+
+
+def run_command(command, error_str):
+    p1 = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p1.communicate()
+    status = p1.returncode
+    if status != 0:
+        logger.error(error_str)
+        logger.debug('stdout:\n%s', stdout)
+        logger.debug('stderr:\n%s', stderr)
+        raise Exception
+
+
+def hpss_transfer(hpss, file_path, transfer_type, keep=None):
+    if hpss == 'none':
+        return
+    if transfer_type == 'put':
+        transfer_word = 'to'
+        transfer_command = 'put'
+    elif transfer_type == 'get':
+        transfer_word = 'from'
+        transfer_command = 'get'
+    else:
+        raise Exception('Invalid transfer_type={}'.format(transfer_type))
+    logger.info('Transferring file {} HPSS: {}'.format(transfer_word, file_path))
+    path, name = os.path.split(file_path)
+
+    # Need to be in local directory for hsi put to work
+    cwd = os.getcwd()
+    if path != '':
+        if (transfer_type == 'get') and (not os.path.isdir(path)):
+            os.makedirs(path)
+        os.chdir(path)
+
+    # Transfer file using hsi put
+    command = 'hsi -q "cd {}; {} {}"'.format(hpss, transfer_command, name)
+    error_str = 'Transferring file {} HPSS: {}'.format(transfer_word, name)
+    run_command(command, error_str)
+
+    # Back to original working directory
+    if path != '':
+        os.chdir(cwd)
+
+    if transfer_type == 'put':
+        # Remove local file if requested
+        if not keep:
+            os.remove(file_path)
 
 
 def hpss_put(hpss, file, keep=True):
     """
     Put a file to the HPSS archive.
     """
-
-    logger.info('Transferring file to HPSS: %s' % (file))
-    path, name = os.path.split(file)
-
-    # Need to be in local directory for hsi put to work
-    cwd = os.getcwd()
-    if path != '':
-        os.chdir(path)
-
-    # Transfer file using hsi put
-    cmd = 'hsi -q "cd %s; put %s"' % (hpss, name)
-    p1 = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
-    (stdout, stderr) = p1.communicate()
-    status = p1.returncode
-    if status != 0:
-        logger.error('Transferring file to HPSS: %s' % (name))
-        logger.debug('stdout:\n%s', stdout)
-        logger.debug('stderr:\n%s', stderr)
-        raise Exception
-
-    # Back to original working directory
-    if path != '':
-        os.chdir(cwd)
-
-    # Remove local file if requested
-    if not keep:
-        os.remove(file)
+    hpss_transfer(hpss, file, 'put', keep)
 
 
 def hpss_get(hpss, file):
     """
-    Get ia file from the HPSS archive.
+    Get a file from the HPSS archive.
     """
+    hpss_transfer(hpss, file, 'get', False)
 
-    logger.info('Transferring from HPSS: %s' % (file))
-    path, name = os.path.split(file)
-
-    # Need to be in local directory for hsi get to work
-    cwd = os.getcwd()
-    if path != '':
-        if not os.path.isdir(path):
-            os.makedirs(path)
-        os.chdir(path)
-
-    # Transfer file using hsi get
-    cmd = 'hsi -q "cd %s; get %s"' % (hpss, name)
-    p1 = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
-    (stdout, stderr) = p1.communicate()
-    status = p1.returncode
-    if status != 0:
-        logger.error('Transferring file from HPSS: %s' % (name))
-        logger.debug('stdout:\n%s', stdout)
-        logger.debug('stderr:\n%s', stderr)
-        raise Exception
-
-    # Back to original working directory
-    if path != '':
-        os.chdir(cwd)
 
 def hpss_chgrp(hpss, group, recurse=False):
     """
     Change the group of the HPSS archive.
     """
+    if hpss == 'none':
+        return
     if recurse:
-        cmd = 'hsi chgrp -R {} {}'.format(group, hpss)
+        recurse_str = '-R '
     else:
-        cmd = 'hsi chgrp {} {}'.format(group, hpss)
-
-    p1 = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
-    (stdout, stderr) = p1.communicate()
-    status = p1.returncode
-    if status != 0:
-        logger.error('Changing group of HPSS archive {} to {}'.format(hpss, group))
-        logger.debug('stdout:\n%s', stdout)
-        logger.debug('stderr:\n%s', stderr)
-        raise Exception
-
+        recurse_str = ''
+    command = 'hsi chgrp {}{} {}'.format(recurse_str, group, hpss)
+    error_str = 'Changing group of HPSS archive {} to {}'.format(hpss, group)
+    run_command(command, error_str)
