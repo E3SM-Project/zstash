23de9b2d79b5b9ca93ff3606753f1da94712b7a0
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-01-27 14:12:11 -0700

    Make `conda/meta.yaml` and `setup.py` consistent
    Inconsistencies in `setup.py` are causing `pip check` to fail
    on the conda package.

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ setup(
     author_email="forsyth2@llnl.gov, golaz1@llnl.gov, shaheen2@llnl.gov",
     description="Long term HPSS archiving software for E3SM",
     packages=find_packages(include=["zstash", "zstash.*"]),
+    python_requires=">=3.6",
     install_requires=[
-        "six==1.16.0",
-        "globus-sdk<4.0.0>=3.0.0",
-        "fair-research-login==0.2.6",
+        "fair-research-login>=0.2.6,<0.3.0",
+        "globus-sdk>=3.0.0,<4.0.0",
+        "six",
     ],
     entry_points={"console_scripts": ["zstash=zstash.main:main"]},
 )
