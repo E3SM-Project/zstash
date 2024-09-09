bded1f308d9e8d44aa9192eefebaac99c223541b
Althea Denlinger <altheaden@unm.edu>
2024-09-09 16:46:15 -0700

    Update setup python version

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email="forsyth2@llnl.gov, golaz1@llnl.gov, shaheen2@llnl.gov",
     description="Long term HPSS archiving software for E3SM",
     packages=find_packages(include=["zstash", "zstash.*"]),
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     entry_points={"console_scripts": ["zstash=zstash.main:main"]},
 )
