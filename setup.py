9c047adfbd66e286b62457ec19be1d304615affc
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2025-01-14 14:39:43 -0800

    Bump to 1.4.4rc1 (#359)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="zstash",
-    version="1.4.3",
+    version="1.4.4rc1",
     author="Ryan Forsyth, Chris Golaz, Zeshawn Shaheen",
     author_email="forsyth2@llnl.gov, golaz1@llnl.gov, shaheen2@llnl.gov",
     description="Long term HPSS archiving software for E3SM",
