085a9bc5aa8e9745990ea23a2657499271b10e7f
Ryan Forsyth <forsyth2@llnl.gov>
2025-04-10 11:16:51 -0700

    Move helper functions in file

--- zstash/globus.py
+++ zstash/globus.py
@@ -14,6 +14,8 @@ from globus_sdk import TransferAPIError, TransferClient, TransferData
 from .settings import logger
 from .utils import GlobusInfo, ts_utc
 
+# Constants ###################################################################
+
 ZSTASH_CLIENT_ID = "6c1629cf-446c-49e7-af95-323c6412397f"
 
 HPSS_ENDPOINT_MAP = {
@@ -42,6 +44,8 @@ ENDPOINT_TO_NAME_MAP = {
     "9cd89cfd-6d04-11e5-ba46-22000b92c6ec": "NERSC HPSS",
     "de463ec4-6d04-11e5-ba46-22000b92c6ec": "Invalid, presumably ALCF HPSS",
 }
+
+# Helper functions ############################################################
 
 
 def ep_to_name(endpoint_id: str) -> str:
@@ -78,6 +82,68 @@ def check_endpoint_version_5(globus_info: GlobusInfo, ep_id):
     return False
 
 
+# Used exclusively in globus_transfer
+def file_exists(globus_info: GlobusInfo, name: str) -> bool:
+    if not globus_info.archive_directory_listing:
+        raise ValueError("archive_directory_listing is undefined")
+    for entry in globus_info.archive_directory_listing:
+        if entry.get("name") == name:
+            return True
+    return False
+
+
+# Used exclusively in globus_transfer
+def globus_block_wait(
+    globus_info: GlobusInfo, wait_timeout: int, polling_interval: int, max_retries: int
+):
+
+    # poll every "polling_interval" seconds to speed up small transfers.  Report every 2 hours, stop waiting aftert 5*2 = 10 hours
+    logger.info(
+        f"{ts_utc()}: BLOCKING START: invoking task_wait for task_id = {globus_info.task_id}"
+    )
+    task_status = "UNKNOWN"
+    retry_count = 0
+    while retry_count < max_retries:
+        try:
+            # Wait for the task to complete
+            logger.info(
+                f"{ts_utc()}: on task_wait try {retry_count+1} out of {max_retries}"
+            )
+            if not globus_info.transfer_client:
+                raise ValueError("transfer_client is undefined")
+            globus_info.transfer_client.task_wait(
+                globus_info.task_id, timeout=wait_timeout, polling_interval=10
+            )
+            logger.info(f"{ts_utc()}: done with wait")
+        except Exception as e:
+            logger.error(f"Unexpected Exception: {e}")
+        else:
+            if not globus_info.transfer_client:
+                raise ValueError("transfer_client is undefined")
+            curr_task = globus_info.transfer_client.get_task(globus_info.task_id)
+            task_status = curr_task["status"]
+            if task_status == "SUCCEEDED":
+                break
+        finally:
+            retry_count += 1
+            logger.info(
+                f"{ts_utc()}: BLOCKING retry_count = {retry_count} of {max_retries} of timeout {wait_timeout} seconds"
+            )
+
+    if retry_count == max_retries:
+        logger.info(
+            f"{ts_utc()}: BLOCKING EXHAUSTED {max_retries} of timeout {wait_timeout} seconds"
+        )
+        task_status = "EXHAUSTED_TIMEOUT_RETRIES"
+
+    logger.info(
+        f"{ts_utc()}: BLOCKING ENDS: task_id {globus_info.task_id} returned from task_wait with status {task_status}"
+    )
+
+    return task_status
+
+
+# Used exclusively in globus_transfer, globus_finalize
 def submit_transfer_with_checks(globus_info: GlobusInfo):
     if not globus_info.transfer_client:
         raise ValueError("transfer_client is undefined")
@@ -106,6 +172,58 @@ def submit_transfer_with_checks(globus_info: GlobusInfo):
                 print("Error is in authorization parameters")
             raise err
     return task
+
+
+# Used exclusively in globus_transfer, globus_finalize
+def globus_wait(globus_info: GlobusInfo, alternative_task_id=None):
+    if alternative_task_id:
+        task_id = alternative_task_id
+    else:
+        task_id = globus_info.task_id
+    try:
+        """
+        A Globus transfer job (task) can be in one of the three states:
+        ACTIVE, SUCCEEDED, FAILED. The script every 20 seconds polls a
+        status of the transfer job (task) from the Globus Transfer service,
+        with 20 second timeout limit. If the task is ACTIVE after time runs
+        out 'task_wait' returns False, and True otherwise.
+        """
+        if not globus_info.transfer_client:
+            raise ValueError("transfer_client is undefined")
+        while not globus_info.transfer_client.task_wait(
+            task_id, timeout=300, polling_interval=20
+        ):
+            pass
+        """
+        The Globus transfer job (task) has been finished (SUCCEEDED or FAILED).
+        Check if the transfer SUCCEEDED or FAILED.
+        """
+        if not globus_info.transfer_client:
+            raise ValueError("transfer_client is undefined")
+        task = globus_info.transfer_client.get_task(task_id)
+        if task["status"] == "SUCCEEDED":
+            src_ep = task["source_endpoint_id"]
+            dst_ep = task["destination_endpoint_id"]
+            label = task["label"]
+            logger.info(
+                f"Globus transfer {task_id}, from {src_ep} to {dst_ep}: {label} succeeded"
+            )
+        else:
+            logger.error("Transfer FAILED")
+    except TransferAPIError as e:
+        if e.code == "NoCredException":
+            logger.error(
+                f"{e.message}. Please go to https://app.globus.org/endpoints and activate the endpoint."
+            )
+        else:
+            logger.error(e)
+        sys.exit(1)
+    except Exception as e:
+        logger.error(f"Exception: {e}")
+        sys.exit(1)
+
+
+# Primary functions ###########################################################
 
 
 def globus_activate(globus_info: GlobusInfo, alt_hpss: str = ""):
@@ -192,15 +310,6 @@ def globus_activate(globus_info: GlobusInfo, alt_hpss: str = ""):
                 f"The {ep_id} endpoint is not activated or the current activation expires soon. Please go to https://app.globus.org/file-manager/collections/{ep_id} and (re)activate the endpoint."
             )
             sys.exit(1)
-
-
-def file_exists(globus_info: GlobusInfo, name: str) -> bool:
-    if not globus_info.archive_directory_listing:
-        raise ValueError("archive_directory_listing is undefined")
-    for entry in globus_info.archive_directory_listing:
-        if entry.get("name") == name:
-            return True
-    return False
 
 
 # C901 'globus_transfer' is too complex (20)
@@ -346,104 +455,6 @@ def globus_transfer(  # noqa: C901
         globus_wait(globus_info)
 
     return task_status
-
-
-def globus_block_wait(
-    globus_info: GlobusInfo, wait_timeout: int, polling_interval: int, max_retries: int
-):
-
-    # poll every "polling_interval" seconds to speed up small transfers.  Report every 2 hours, stop waiting aftert 5*2 = 10 hours
-    logger.info(
-        f"{ts_utc()}: BLOCKING START: invoking task_wait for task_id = {globus_info.task_id}"
-    )
-    task_status = "UNKNOWN"
-    retry_count = 0
-    while retry_count < max_retries:
-        try:
-            # Wait for the task to complete
-            logger.info(
-                f"{ts_utc()}: on task_wait try {retry_count+1} out of {max_retries}"
-            )
-            if not globus_info.transfer_client:
-                raise ValueError("transfer_client is undefined")
-            globus_info.transfer_client.task_wait(
-                globus_info.task_id, timeout=wait_timeout, polling_interval=10
-            )
-            logger.info(f"{ts_utc()}: done with wait")
-        except Exception as e:
-            logger.error(f"Unexpected Exception: {e}")
-        else:
-            if not globus_info.transfer_client:
-                raise ValueError("transfer_client is undefined")
-            curr_task = globus_info.transfer_client.get_task(globus_info.task_id)
-            task_status = curr_task["status"]
-            if task_status == "SUCCEEDED":
-                break
-        finally:
-            retry_count += 1
-            logger.info(
-                f"{ts_utc()}: BLOCKING retry_count = {retry_count} of {max_retries} of timeout {wait_timeout} seconds"
-            )
-
-    if retry_count == max_retries:
-        logger.info(
-            f"{ts_utc()}: BLOCKING EXHAUSTED {max_retries} of timeout {wait_timeout} seconds"
-        )
-        task_status = "EXHAUSTED_TIMEOUT_RETRIES"
-
-    logger.info(
-        f"{ts_utc()}: BLOCKING ENDS: task_id {globus_info.task_id} returned from task_wait with status {task_status}"
-    )
-
-    return task_status
-
-
-def globus_wait(globus_info: GlobusInfo, alternative_task_id=None):
-    if alternative_task_id:
-        task_id = alternative_task_id
-    else:
-        task_id = globus_info.task_id
-    try:
-        """
-        A Globus transfer job (task) can be in one of the three states:
-        ACTIVE, SUCCEEDED, FAILED. The script every 20 seconds polls a
-        status of the transfer job (task) from the Globus Transfer service,
-        with 20 second timeout limit. If the task is ACTIVE after time runs
-        out 'task_wait' returns False, and True otherwise.
-        """
-        if not globus_info.transfer_client:
-            raise ValueError("transfer_client is undefined")
-        while not globus_info.transfer_client.task_wait(
-            task_id, timeout=300, polling_interval=20
-        ):
-            pass
-        """
-        The Globus transfer job (task) has been finished (SUCCEEDED or FAILED).
-        Check if the transfer SUCCEEDED or FAILED.
-        """
-        if not globus_info.transfer_client:
-            raise ValueError("transfer_client is undefined")
-        task = globus_info.transfer_client.get_task(task_id)
-        if task["status"] == "SUCCEEDED":
-            src_ep = task["source_endpoint_id"]
-            dst_ep = task["destination_endpoint_id"]
-            label = task["label"]
-            logger.info(
-                f"Globus transfer {task_id}, from {src_ep} to {dst_ep}: {label} succeeded"
-            )
-        else:
-            logger.error("Transfer FAILED")
-    except TransferAPIError as e:
-        if e.code == "NoCredException":
-            logger.error(
-                f"{e.message}. Please go to https://app.globus.org/endpoints and activate the endpoint."
-            )
-        else:
-            logger.error(e)
-        sys.exit(1)
-    except Exception as e:
-        logger.error(f"Exception: {e}")
-        sys.exit(1)
 
 
 def globus_finalize(globus_info: GlobusInfo, non_blocking: bool = False):
